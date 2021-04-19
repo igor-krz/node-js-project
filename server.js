0d925ab1561d0160d63f13b52b29ea46982d9cb8
igor <krzywakigor@gmail.com>
2021-04-20 00:33:30 +0100

    CRUD functionality implemented

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
-const dotenv = require('dotenv').config({ path: './config/config.env' });
+const dotenv = require('dotenv');
+const logger = require('./middleware/logger.js');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db.js');
+
+// Load env variables
+dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps.js');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
+// Dev logging middleware
+if ((process.env.NODE_ENV = 'development')) {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.bold
+  )
 );
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
